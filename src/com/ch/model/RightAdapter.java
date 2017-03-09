6e833a2d29b75adba3759818c8ad4b5754ef7f18
caohanandyuci <283190253@qq.com>
2017-03-09 14:05:47 +0800

    Update RightAdapter.java
    fdsafda

--- src/com/ch/model/RightAdapter.java
+++ src/com/ch/model/RightAdapter.java
@@ -16,40 +16,68 @@ import android.widget.TextView;
 
 
 
-public class RightAdapter extends BaseAdapter {
+public class RightAdapter extends BaseAdapter implements StickyListHeadersAdapter, SectionIndexer {
 
-	private List<Product> list = null;
-    private Context mContext;
+	private final Context mContext;
+	private String[] mCountries;
+	private int[] mSectionIndices;
+	private Character[] mSectionLetters;
+	private LayoutInflater mInflater;
 
-    public RightAdapter(Context mContext, List<Product> list) {
-        this.mContext = mContext;
-        this.list = list;
-    }
+	private List<Product> mProducts = null;
+	
+	public RightAdapter(Context context,List<Product> products) {
+		mContext = context;
+		mInflater = LayoutInflater.from(context);
+		mProducts = products;
+		mCountries = context.getResources().getStringArray(R.array.countries);
+		mSectionIndices = getSectionIndices();
+		mSectionLetters = getSectionLetters();
+	}
 
+	private int[] getSectionIndices() {
+		ArrayList<Integer> sectionIndices = new ArrayList<Integer>();
+		char lastFirstChar = mCountries[0].charAt(0);
+		sectionIndices.add(0);
+		for (int i = 1; i < mCountries.length; i++) {
+			if (mCountries[i].charAt(0) != lastFirstChar) {
+				lastFirstChar = mCountries[i].charAt(0);
+				sectionIndices.add(i);
+			}
+		}
+		int[] sections = new int[sectionIndices.size()];
+		for (int i = 0; i < sectionIndices.size(); i++) {
+			sections[i] = sectionIndices.get(i);
+		}
+		return sections;
+	}
 
-    @Override
-    public int getCount() {
-        // TODO Auto-generated method stub
-        return this.list.size();
-    }
+	private Character[] getSectionLetters() {
+		Character[] letters = new Character[mSectionIndices.length];
+		for (int i = 0; i < mSectionIndices.length; i++) {
+			letters[i] = mCountries[mSectionIndices[i]].charAt(0);
+		}
+		return letters;
+	}
 
-    @Override
-    public Object getItem(int position) {
-        // TODO Auto-generated method stub
-        return list.get(position);
-    }
+	@Override
+	public int getCount() {
+		return mCountries.length;
+	}
 
-    @Override
-    public long getItemId(int position) {
-        // TODO Auto-generated method stub
-        return position;
-    }
+	@Override
+	public Object getItem(int position) {
+		return mCountries[position];
+	}
 
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
 
-	@SuppressLint("NewApi") @Override
+	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		convertView=LayoutInflater.from(mContext).inflate(R.layout.product,null);
+		convertView = LayoutInflater.from(mContext).inflate(R.layout.product, null);
 		ImageView imageView = (ImageView) convertView.findViewById(R.id.productbg);
 		ImageView addviewImageView = (ImageView) convertView.findViewById(R.id.addbg);
 		addviewImageView.setBackground(mContext.getResources().getDrawable(R.drawable.add));
@@ -57,12 +85,79 @@ public class RightAdapter extends BaseAdapter {
 		subviewImageView.setBackground(mContext.getResources().getDrawable(R.drawable.sub));
 		imageView.setImageDrawable(mContext.getResources().getDrawable(R.drawable.timg));
 		return convertView;
+
 	}
 
+	@Override
+	public View getHeaderView(int position, View convertView, ViewGroup parent) {
+		HeaderViewHolder holder;
 
-	private Context getResources() {
-		// TODO Auto-generated method stub
-		return null;
+		if (convertView == null) {
+			holder = new HeaderViewHolder();
+			convertView = mInflater.inflate(R.layout.header, parent, false);
+			holder.text = (TextView) convertView.findViewById(R.id.text1);
+			convertView.setTag(holder);
+		} else {
+			holder = (HeaderViewHolder) convertView.getTag();
+		}
+
+		//CharSequence headerChar = mCountries[position].subSequence(0, 1);
+		//holder.text.setText(headerChar);
+
+		String category = "this is category:"+mProducts.get(position).mCategory;
+		holder.text.setText(category);
+		return convertView;
 	}
 
-}
+	@Override
+	public long getHeaderId(int position) {
+		//return mCountries[position].subSequence(0, 1).charAt(0);
+		return mProducts.get(position).mCategory;
+	}
+
+	@Override
+	public int getPositionForSection(int section) {
+		if (section >= mSectionIndices.length) {
+			section = mSectionIndices.length - 1;
+		} else if (section < 0) {
+			section = 0;
+		}
+		return mSectionIndices[section];
+	}
+
+	@Override
+	public int getSectionForPosition(int position) {
+		for (int i = 0; i < mSectionIndices.length; i++) {
+			if (position < mSectionIndices[i]) {
+				return i - 1;
+			}
+		}
+		return mSectionIndices.length - 1;
+	}
+
+	@Override
+	public Object[] getSections() {
+		return mSectionLetters;
+	}
+
+	public void clear() {
+		mCountries = new String[0];
+		mSectionIndices = new int[0];
+		mSectionLetters = new Character[0];
+		notifyDataSetChanged();
+	}
+
+	public void restore() {
+		mCountries = mContext.getResources().getStringArray(R.array.countries);
+		mSectionIndices = getSectionIndices();
+		mSectionLetters = getSectionLetters();
+		notifyDataSetChanged();
+	}
+
+	class HeaderViewHolder {
+		TextView text;
+	}
+
+	class ViewHolder {
+		TextView text;
+	}
